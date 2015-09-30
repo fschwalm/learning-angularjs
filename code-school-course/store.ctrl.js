f29df95638c0aa932b4bbcde781196fb2024d3b8
fagner <fagnerschwalm@gmail.com>
2015-09-30 20:03:36 -0300

    Add store controller.

---
+++ code-school-course/store.ctrl.js
@@ -0,0 +1,9 @@
+angular.module('store').controller('StoreCtrl', function() {
+    this.product = gem;
+});
+var gem = {
+    name: 'Crystal',
+    price: 2.95,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+    canPurchase: true
+}
