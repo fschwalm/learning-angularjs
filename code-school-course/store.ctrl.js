0970cd1237462742d428bfb795d60c7720a6f699
Fagner Schwalm <fagnerschwalm@gmail.com>
2015-10-01 00:36:15 -0300

    Add gems and ng-repeat.

--- code-school-course/store.ctrl.js
+++ code-school-course/store.ctrl.js
@@ -1,9 +1,19 @@
 angular.module('store').controller('StoreCtrl', function() {
-    this.product = gem;
+    this.products = gems;
 });
-var gem = {
+var gems = [{
     name: 'Crystal',
     price: 2.95,
     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
     canPurchase: true
-}
+}, {
+    name: 'Ruby',
+    price: 2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+    canPurchase: true
+}, {
+    name: 'Yellow gem',
+    price: 5,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+    canPurchase: true
+}];
