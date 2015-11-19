4164e0f08db3c1c47c97ed0ec51d44fc4be0b6fa
fagner <fagnerschwalm@gmail.com>
2015-11-19 13:50:52 -0200

    Extract products to json file. And change control to use http service

--- code-school-course/store.ctrl.js
+++ code-school-course/store.ctrl.js
@@ -1,56 +1,7 @@
-angular.module('store').controller('StoreCtrl', function() {
-    this.products = gems;
-});
-var gems = [{
-    name: 'Crystal',
-    price: 2.95,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
-    canPurchase: true,
-    images: [{
-        full: 'img/blue-gem.png'
-    }],
-    reviews: [ { 
-        stars: 5,
-        body: "I love this product!",
- author: "joe@thomas.com" 
-    },  { 
-        stars: 1,
-        body: "This product sucks",
- author: "tim@hater.com" 
-    } ]
-
-}, {
-    name: 'Ruby',
-    price: 2,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
-    canPurchase: true,
-    images: [{
-        full: 'img/red-gem.png'
-    }],
-    reviews: [ { 
-        stars: 5,
-        body: "I love this product!",
- author: "joe@thomas.com" 
-    },  { 
-        stars: 1,
-        body: "This product sucks",
- author: "tim@hater.com" 
-    } ]
-}, {
-    name: 'Green gem',
-    price: 5,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
-    canPurchase: true,
-    images: [{
-        full: 'img/green-gem.png'
-    }],
-    reviews: [ { 
-        stars: 5,
-        body: "I love this product!",
- author: "joe@thomas.com" 
-    },  { 
-        stars: 1,
-        body: "This product sucks",
- author: "tim@hater.com" 
-    } ]
-}];
+angular.module('store').controller('StoreCtrl', ['$http', function($http) {
+    var store = this;
+    this.products = [];
+    $http.get('products.json').success(function(data){
+        store.products = data;
+    });
+}]);
