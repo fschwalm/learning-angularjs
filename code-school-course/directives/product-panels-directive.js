9036a1bdb362ac68994a59cd802f414e91bdae4d
fagner <fagnerschwalm@gmail.com>
2015-11-19 12:46:48 -0200

    Create panels directive with controller

---
+++ code-school-course/directives/product-panels-directive.js
@@ -0,0 +1,8 @@
+angular.module('store').directive('productPanels', function(){
+    return {
+        restrict : 'E',
+        templateUrl : 'templates/product-panels.html',
+        controller: 'PanelController',
+        controllerAs : 'panel'
+    };
+});
